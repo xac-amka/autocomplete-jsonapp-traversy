fac51e63540d4683e9040875e92ce38e42f8a6eb
Proboss <64521451+xac-amka@users.noreply.github.com>
2020-08-06 11:58:15 +0800

    initial comment

---
+++ README.md
@@ -0,0 +1,5 @@
+# Quick Autocomplete App With JS & JSON
+
+## Tools
+ - [https://bootswatch.com/](https://bootswatch.com/)
+ - [json file](https://gist.github.com/bradtraversy/20dee7787486d10db3bd1f55fae5fdf4)
---
+++ js/main.js
@@ -0,0 +1,42 @@
+const search = document.getElementById("search");
+const matchList = document.getElementById("match-list");
+
+search.addEventListener("input", ()=>{
+   searchStates(search.value); 
+});
+
+// Search json file and filter it
+const searchStates = async (searchText) => {
+    const res = await fetch("../state_capitals.json");
+    const states = await res.json();
+
+    // Get matches to current text input
+    let matches = states.filter(state => {
+        const regex = new RegExp(`^${searchText}`, 'gi');
+        return state.name.match(regex) || state.abbr.match(regex);
+    })
+
+    if(searchText.length === 0) {
+        matches = [];
+        matchList.innerHTML = '';
+    }
+
+    outputHtml(matches);
+}
+
+// Show results in HTML
+const outputHtml = matches => {
+    if(matches.length > 0 ){
+        const html = matches.map(
+            match => `
+                <div class="card card-body mb-4">
+                    <h4>${match.name} (${match.abbr}) <span class="text-primary">${match.capital}</span></h4>
+                    <small>Lat: ${match.lat} / Long: ${match.long}</small>
+                </div>
+            `
+        ).join('');
+
+        matchList.innerHTML = html;
+        // console.log(html);
+    }
+}
